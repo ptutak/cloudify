8694beaf084137bd0ea2d9659fe52ac935272845
Piotr Tutak <p.tutak@outlook.com>
2019-09-16 23:26:25 +0200

    testing

--- tests/test_amartus.py
+++ tests/test_amartus.py
@@ -1,7 +1,15 @@
-import amartus
+from amartus import main_app
 from unittest import TestCase
 
 
 class FirstTestCase(TestCase):
-    def test_first(self):
+    def setUp(self):
+        self.app = main_app.test_client()
+        self.app.testing = True
+
+    def tearDown(self):
         pass
+
+    def test_main_route(self):
+        result = self.app.get('/')
+        self.assertEqual(result.status_code, 404)
