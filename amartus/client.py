c688756f8e70c1c3e83a0ac08a74214ba1f3f6f4
Piotr Tutak <p.tutak@outlook.com>
2019-09-18 00:30:07 +0200

    finished

---
+++ amartus/client.py
@@ -0,0 +1,60 @@
+import sys
+import argparse
+import socket
+import json
+from uuid import getnode
+from urllib.request import urlopen, Request
+from urllib.parse import urljoin
+
+
+def parse_args(args=sys.argv[1:]):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'server_address',
+        help='Server address'
+    )
+    parser.add_argument(
+        '-n',
+        '--name',
+        help='Name',
+        default=''
+    )
+    parser.add_argument(
+        '-u',
+        '--userdata',
+        help='Userdata',
+        default=''
+    )
+    return parser.parse_args(args)
+
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(('10.255.255.255', 1))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = '127.0.0.1'
+    finally:
+        s.close()
+    return ip
+
+
+def main():
+    args = parse_args()
+    new_register_request = Request(
+        urljoin(args.server_address, '/ztp/register/{}'.format(getnode())),
+        data=json.dumps({
+            "ip": get_ip(),
+            "name": args.name,
+            "userdata": args.userdata
+        }).encode(),
+        method='PUT',
+        headers={'Content-type': 'application/json'}
+    )
+    urlopen(new_register_request)
+    return 0
+
+
+if __name__ == '__main__':
+    exit(main())
---
+++ tests/test_client.py
@@ -0,0 +1,51 @@
+import json
+from unittest import TestCase
+from unittest.mock import patch, MagicMock
+import amartus.client as client
+from amartus.client import getnode, get_ip
+
+
+class TestClient(TestCase):
+    def setUp(self):
+        self.patcher = patch('amartus.client.urlopen')
+        self.mocked_urlopen = self.patcher.start()
+
+    def tearDown(self):
+        self.patcher.stop()
+
+    def test_args(self):
+        args = client.parse_args([
+            'https:/my.local.address',
+            '-n',
+            'Peter',
+            '-u',
+            'Data'])
+        self.assertEqual(args.name, 'Peter')
+        self.assertEqual(args.userdata, 'Data')
+
+    def test_get_ip(self):
+        # Don't really have an idea how to test this functionality
+        pass
+
+    def test_main(self):
+        with patch('amartus.client.parse_args') as mocked_parse:
+            local_args = MagicMock()
+            local_args.name = 'Peter'
+            local_args.userdata = 'Data'
+            local_args.server_address = 'http://my.local.address'
+            mocked_parse.return_value = local_args
+            client.main()
+
+        args, kwargs = self.mocked_urlopen.call_args
+        request = args[0]
+        self.assertEqual(
+            request.full_url,
+            'http://my.local.address/ztp/register/{}'.format(getnode()))
+        self.assertEqual(
+            json.loads(request.data),
+            {'ip': get_ip(), 'name': 'Peter', 'userdata': 'Data'}
+        )
+        self.assertEqual(
+            request.header_items(),
+            [('Content-type', 'application/json')]
+        )
